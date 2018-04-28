1beb52f640a41245f357249b303526ba6f7b9025
Rosa <Rosa@RosadeMacBook-Pro.local>
2018-04-28 00:15:13 -0600

    changed index.js

--- signup_forgotpassword/js/index.js
+++ signup_forgotpassword/js/index.js
@@ -1,12 +1,19 @@
-$(document).on('click', '.SignUp', function (e) {
-  var $input = $('#input');
-  var checked = $('#input').prop('checked');
-  if (!checked) {
-    $input.prop('checked', true);
-  }
+$(document).on('click', '.SignUp', function (e)
+ {
+ 	var $input = $('#input');
+  	var checked = $('#input').prop('checked');
+  if (!checked)
+   {
+    $input.prop('checked', true); /*  this opens up sign up link */
+   }
 });
 
-$(document).on('submit', 'form', function (e) {
+$(document).on('submit', 'form', function (e)
+ {
   e.preventDefault();
-  window.open("file:///Users/Rosa/Desktop/3308-Tech-BuzzWords/LogInPage/Log_In.html");
-});
+  console.log($('#email').val())
+  if($('#email').val() != "" && $('#pass').val() != "") {
+  	window.open("file:///Users/Rosa/Desktop/3308-Tech-BuzzWords/LogInPage/Log_In.html");
+  }
+   
+  });
