1dea32844e372f127b17bdc033d1fe77d37aa41b
rosa637033 <36648437+rosa637033@users.noreply.github.com>
2018-04-28 15:03:39 -0600

    change address

--- signup_forgotpassword/js/index.js
+++ signup_forgotpassword/js/index.js
@@ -13,7 +13,7 @@ $(document).on('submit', 'form', function (e)
   e.preventDefault();
   console.log($('#email').val())
   if($('#email').val() != "" && $('#pass').val() != "") {
-  	window.open("file:///Users/Rosa/Desktop/3308-Tech-BuzzWords/LogInPage/Log_In.html");
+  	window.open("/view/Log_In.html");
   }
    
   });
